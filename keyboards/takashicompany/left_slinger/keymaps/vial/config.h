53009af483693486d174aef14bb0ccddce7bd1e0
takashicompany <t@kashi.company>
2024-11-19 19:27:09 +0900

    left_slinger vial

---
+++ keyboards/takashicompany/left_slinger/keymaps/vial/config.h
@@ -0,0 +1,11 @@
+// Copyright 2024 takashicompany (@takashicompany)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x91, 0x09, 0x6C, 0x31, 0xA4, 0x5A, 0xBA, 0x65}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
