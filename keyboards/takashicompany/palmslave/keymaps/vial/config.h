405b96675e3795994dba78d18c67e81b9cc8be29
takashicompany <t@kashi.company>
2025-09-17 01:30:39 +0900

    palmslave

---
+++ keyboards/takashicompany/palmslave/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+// Copyright 2025 takashicompany (@takashicompany)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 7
+#define VIAL_KEYBOARD_UID {0x84, 0x01, 0xE0, 0xB3, 0x70, 0xFF, 0x2B, 0x7F}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 5 }
